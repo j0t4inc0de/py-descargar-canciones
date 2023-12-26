988a1902987f2c8457beb04e8fdcd27842473b02
j0t4inc0de <juan.erices04@inacapmail.cl>
2023-12-26 19:03:11 -0300

    posicion de botones

--- programa/tubepy.py
+++ programa/tubepy.py
@@ -7,18 +7,21 @@ class App:
     def __init__(self):
         self.ventana = Tk()
         self.ventana.title("Tubepy echo por Jota")
-        self.ventana.geometry("500x300")
+        self.ventana.geometry("500x150")
         self.ventana.resizable(False, False)
         self.formulario()
         self.ventana.mainloop()
 
     def formulario(self):
         self.label2 = Label(self.ventana, text="Tubepy echo por Jota").pack()
+
+        self.separador = Label(self.ventana, text="").pack()
+
         self.label = Label(self.ventana, text="Url de la cancion: ").pack()
         self.link = Entry(self.ventana, width=50)
         self.link.pack()
-        self.boton = Button(self.ventana, text="Descargar", command=self.descargar).pack()
-        self.btnLimpiar = Button(self.ventana, text="Limpiar", command=self.limpiar).pack()
+        self.boton = Button(self.ventana, text="Descargar", command=self.descargar).place(x=100, y=90)
+        self.btnLimpiar = Button(self.ventana, text="Limpiar", command=self.limpiar).place(x=170, y=90)
     def descargar(self):
         url = self.link.get()
         video = YouTube(url)
@@ -30,6 +33,6 @@ class App:
         clip = AudioFileClip(download_path)
         clip.write_audiofile("mp3/"+nombre+".mp3")
         clip.close()
-    
+        self.limpiar()
     def limpiar(self):
         self.link.delete(0, END)
